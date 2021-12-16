2f14de141ac1dee27a7926a743b884e7bc971ac9
Stuart Clark <stuart.clark@umotif.com>
2021-12-16 11:23:40 +0000

    Initial Commit

---
+++ Shared/RayScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  RayScanApp.swift
+//  Shared
+//
+//  Created by Stuart Clark on 16/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct RayScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
